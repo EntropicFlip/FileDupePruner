b50741832867d2541f5fff6383e3c604e46e5bc9
Kain Shin <kaininshin@gmail.com>
2016-04-18 08:30:44 -0400

    Detects and optionally Moves Duplicate Files
    C# Tool using Windows Forms that recursively detects and optionally
    moves duplicate files (as determined by binary comparison ignoring
    filenames) to a _Pruned folder for inspection by a human before deletion

---
+++ FileDupePruner/MainForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace FileDupePruner
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.MoveDupesButton = new System.Windows.Forms.Button();
+			this.pruneProgressBar = new System.Windows.Forms.ProgressBar();
+			this.ProgressLabel = new System.Windows.Forms.Label();
+			this.PrimaryPathTextbox = new System.Windows.Forms.TextBox();
+			this.SecondaryPathTextbox = new System.Windows.Forms.TextBox();
+			this.PrimaryLabel = new System.Windows.Forms.Label();
+			this.SecondaryLabel = new System.Windows.Forms.Label();
+			this.PruneDumpTextbox = new System.Windows.Forms.TextBox();
+			this.PruneDumpLabel = new System.Windows.Forms.Label();
+			this.checkBoxDoNothing = new System.Windows.Forms.CheckBox();
+			this.checkBoxWithinSelf = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// MoveDupesButton
+			// 
+			this.MoveDupesButton.Location = new System.Drawing.Point(472, 297);
+			this.MoveDupesButton.Name = "MoveDupesButton";
+			this.MoveDupesButton.Size = new System.Drawing.Size(189, 33);
+			this.MoveDupesButton.TabIndex = 0;
+			this.MoveDupesButton.Text = "Move Dupes To Another Folder";
+			this.MoveDupesButton.UseVisualStyleBackColor = true;
+			this.MoveDupesButton.Click += new System.EventHandler(this.MoveDupesButton_Click);
+			// 
+			// pruneProgressBar
+			// 
+			this.pruneProgressBar.Location = new System.Drawing.Point(15, 257);
+			this.pruneProgressBar.Name = "pruneProgressBar";
+			this.pruneProgressBar.Size = new System.Drawing.Size(646, 21);
+			this.pruneProgressBar.TabIndex = 1;
+			// 
+			// ProgressLabel
+			// 
+			this.ProgressLabel.AutoSize = true;
+			this.ProgressLabel.Location = new System.Drawing.Point(12, 241);
+			this.ProgressLabel.Name = "ProgressLabel";
+			this.ProgressLabel.Size = new System.Drawing.Size(48, 13);
+			this.ProgressLabel.TabIndex = 2;
+			this.ProgressLabel.Text = "Progress";
+			// 
+			// PrimaryPathTextbox
+			// 
+			this.PrimaryPathTextbox.AllowDrop = true;
+			this.PrimaryPathTextbox.Location = new System.Drawing.Point(15, 75);
+			this.PrimaryPathTextbox.Name = "PrimaryPathTextbox";
+			this.PrimaryPathTextbox.Size = new System.Drawing.Size(646, 20);
+			this.PrimaryPathTextbox.TabIndex = 3;
+			this.PrimaryPathTextbox.TextChanged += new System.EventHandler(this.PrimaryPathTextbox_TextChanged);
+			this.PrimaryPathTextbox.DragDrop += new System.Windows.Forms.DragEventHandler(this.OnDragDrop);
+			this.PrimaryPathTextbox.DragEnter += new System.Windows.Forms.DragEventHandler(this.OnDragEnter);
+			// 
+			// SecondaryPathTextbox
+			// 
+			this.SecondaryPathTextbox.AllowDrop = true;
+			this.SecondaryPathTextbox.Location = new System.Drawing.Point(15, 119);
+			this.SecondaryPathTextbox.Name = "SecondaryPathTextbox";
+			this.SecondaryPathTextbox.Size = new System.Drawing.Size(644, 20);
+			this.SecondaryPathTextbox.TabIndex = 4;
+			this.SecondaryPathTextbox.TextChanged += new System.EventHandler(this.SecondaryPathTextbox_TextChanged);
+			this.SecondaryPathTextbox.DragDrop += new System.Windows.Forms.DragEventHandler(this.OnDragDrop);
+			this.SecondaryPathTextbox.DragEnter += new System.Windows.Forms.DragEventHandler(this.OnDragEnter);
+			// 
+			// PrimaryLabel
+			// 
+			this.PrimaryLabel.AutoSize = true;
+			this.PrimaryLabel.Location = new System.Drawing.Point(12, 50);
+			this.PrimaryLabel.Name = "PrimaryLabel";
+			this.PrimaryLabel.Size = new System.Drawing.Size(134, 13);
+			this.PrimaryLabel.TabIndex = 5;
+			this.PrimaryLabel.Text = "Primary Folder To Preserve";
+			// 
+			// SecondaryLabel
+			// 
+			this.SecondaryLabel.AutoSize = true;
+			this.SecondaryLabel.Location = new System.Drawing.Point(12, 103);
+			this.SecondaryLabel.Name = "SecondaryLabel";
+			this.SecondaryLabel.Size = new System.Drawing.Size(301, 13);
+			this.SecondaryLabel.TabIndex = 6;
+			this.SecondaryLabel.Text = "Secondary Folder to Prune (Drag a folder for easy path setting)";
+			// 
+			// PruneDumpTextbox
+			// 
+			this.PruneDumpTextbox.AllowDrop = true;
+			this.PruneDumpTextbox.Location = new System.Drawing.Point(15, 174);
+			this.PruneDumpTextbox.Name = "PruneDumpTextbox";
+			this.PruneDumpTextbox.Size = new System.Drawing.Size(646, 20);
+			this.PruneDumpTextbox.TabIndex = 7;
+			this.PruneDumpTextbox.DragDrop += new System.Windows.Forms.DragEventHandler(this.OnDragDrop);
+			this.PruneDumpTextbox.DragEnter += new System.Windows.Forms.DragEventHandler(this.OnDragEnter);
+			// 
+			// PruneDumpLabel
+			// 
+			this.PruneDumpLabel.AutoSize = true;
+			this.PruneDumpLabel.Location = new System.Drawing.Point(12, 158);
+			this.PruneDumpLabel.Name = "PruneDumpLabel";
+			this.PruneDumpLabel.Size = new System.Drawing.Size(132, 13);
+			this.PruneDumpLabel.TabIndex = 8;
+			this.PruneDumpLabel.Text = "Place to dump pruned files";
+			// 
+			// checkBoxDoNothing
+			// 
+			this.checkBoxDoNothing.AutoSize = true;
+			this.checkBoxDoNothing.Checked = true;
+			this.checkBoxDoNothing.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBoxDoNothing.Location = new System.Drawing.Point(15, 12);
+			this.checkBoxDoNothing.Name = "checkBoxDoNothing";
+			this.checkBoxDoNothing.Size = new System.Drawing.Size(177, 17);
+			this.checkBoxDoNothing.TabIndex = 9;
+			this.checkBoxDoNothing.Text = "Preview Only (generate log files)";
+			this.checkBoxDoNothing.UseVisualStyleBackColor = true;
+			this.checkBoxDoNothing.CheckedChanged += new System.EventHandler(this.checkBoxDoNothing_CheckedChanged);
+			// 
+			// checkBoxWithinSelf
+			// 
+			this.checkBoxWithinSelf.AutoSize = true;
+			this.checkBoxWithinSelf.Location = new System.Drawing.Point(283, 12);
+			this.checkBoxWithinSelf.Name = "checkBoxWithinSelf";
+			this.checkBoxWithinSelf.Size = new System.Drawing.Size(216, 17);
+			this.checkBoxWithinSelf.TabIndex = 10;
+			this.checkBoxWithinSelf.Text = "Check for Duplicates within Primary Only";
+			this.checkBoxWithinSelf.UseVisualStyleBackColor = true;
+			this.checkBoxWithinSelf.CheckedChanged += new System.EventHandler(this.checkBoxWithinSelf_CheckedChanged);
+			// 
+			// MainForm
+			// 
+			this.AcceptButton = this.MoveDupesButton;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(683, 363);
+			this.Controls.Add(this.checkBoxWithinSelf);
+			this.Controls.Add(this.checkBoxDoNothing);
+			this.Controls.Add(this.PruneDumpLabel);
+			this.Controls.Add(this.PruneDumpTextbox);
+			this.Controls.Add(this.SecondaryLabel);
+			this.Controls.Add(this.PrimaryLabel);
+			this.Controls.Add(this.SecondaryPathTextbox);
+			this.Controls.Add(this.PrimaryPathTextbox);
+			this.Controls.Add(this.ProgressLabel);
+			this.Controls.Add(this.pruneProgressBar);
+			this.Controls.Add(this.MoveDupesButton);
+			this.Name = "MainForm";
+			this.Text = "File Dupe Pruner";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button MoveDupesButton;
+		private System.Windows.Forms.ProgressBar pruneProgressBar;
+		private System.Windows.Forms.Label ProgressLabel;
+		private System.Windows.Forms.TextBox PrimaryPathTextbox;
+		private System.Windows.Forms.TextBox SecondaryPathTextbox;
+		private System.Windows.Forms.Label PrimaryLabel;
+		private System.Windows.Forms.Label SecondaryLabel;
+		private System.Windows.Forms.TextBox PruneDumpTextbox;
+		private System.Windows.Forms.Label PruneDumpLabel;
+		private System.Windows.Forms.CheckBox checkBoxDoNothing;
+		private System.Windows.Forms.CheckBox checkBoxWithinSelf;
+	}
+}
+
---
+++ FileDupePruner/MainForm.cs
@@ -0,0 +1,418 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileDupePruner
+{
+	public partial class MainForm : Form
+	{
+		List<TextBox> m_textBoxes = new List<TextBox>();
+
+		/////////////////////////////////////////////////////////////////////////////
+		public MainForm()
+		{
+			InitializeComponent();
+
+			m_textBoxes.Clear();
+			m_textBoxes.Add(PrimaryPathTextbox);
+			m_textBoxes.Add(SecondaryPathTextbox);
+			m_textBoxes.Add(PruneDumpTextbox);
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			OnWithinSelfChanged();
+			OnDoNothingChanged();
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		static int CalculateIterations(int n)
+		{
+			if (n <= 1)
+			{
+				return 1;
+			}
+
+			int result = 0;
+			for (int i = 1; i < n; ++i)
+			{
+				result += i;
+			}
+			return result;
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		private void MoveDupesButton_Click(object sender, EventArgs e)
+		{
+			//Get Paths...
+			string primaryPath = PrimaryPathTextbox.Text;
+			if (0 == primaryPath.Length)
+			{
+				MessageBox.Show(this, "The Primary Folder needs to be specified");
+				return;
+			}
+			if (!Directory.Exists(primaryPath))
+			{
+				MessageBox.Show(this, "The Primary Folder does not exist");
+				return;
+			}
+
+			bool withinSelf = checkBoxWithinSelf.Checked;
+			string secondaryPath = SecondaryPathTextbox.Text;
+			if (!withinSelf)
+			{
+				if (0 == secondaryPath.Length)
+				{
+					MessageBox.Show(this, "The Secondary Folder needs to be specified unless you are just checking within the Primary folder");
+					return;
+				}
+				if (!Directory.Exists(secondaryPath))
+				{
+					MessageBox.Show(this, "The Secondary Folder does not exist");
+					return;
+				}
+			}
+
+			string pruneDumpPath = PruneDumpTextbox.Text;
+			if (0 == pruneDumpPath.Length)
+			{
+				MessageBox.Show(this, "The Prune Dump Folder needs to be specified");
+				return;
+			}
+			if (!Directory.Exists(pruneDumpPath))
+			{
+				Directory.CreateDirectory(pruneDumpPath);
+			}
+
+			//Set up log...
+			string prunedFilesLogName = pruneDumpPath + "\\PruneLog.log";
+			StreamWriter logWriter = File.CreateText(prunedFilesLogName);
+			logWriter.AutoFlush = true;
+
+			if (withinSelf)
+			{
+				logWriter.WriteLine("=== FILES THAT CAN BE PRUNED " + System.DateTime.Now.ToString() + " ===");
+			}
+			else
+			{
+				logWriter.WriteLine("=== PRUNED FILES " + System.DateTime.Now.ToString() + " ===");
+			}
+
+			//Gather files...
+			List<string> primaryFilenames = new List<string>();
+			GatherAllFiles(primaryPath, ref primaryFilenames);
+			int numPrimaryFiles = primaryFilenames.Count;
+
+			List<string> secondaryFilenames = new List<string>();
+			if (!withinSelf)
+			{
+				GatherAllFiles(secondaryPath, ref secondaryFilenames);
+			}
+
+			//Set up the progress bar
+			pruneProgressBar.Value = 0;
+			pruneProgressBar.Step = 1;
+			pruneProgressBar.Minimum = 0;
+			int maxSteps = withinSelf
+						? CalculateIterations(numPrimaryFiles)
+						: numPrimaryFiles * secondaryFilenames.Count;
+			pruneProgressBar.Maximum = maxSteps;
+
+			//Compare files...
+			List<string> filesToPrune = new List<string>();
+			FileStream primaryFileStream, secondaryFileStream;
+			if (withinSelf)
+			{
+				string primaryFile, secondaryFile;
+				for (int i = 0; i < numPrimaryFiles-1; ++i)
+				{
+					primaryFile = primaryFilenames[i];
+					primaryFileStream = new FileStream(primaryFile, FileMode.Open);
+					for (int j = (i+1); j < numPrimaryFiles; ++j)
+					{
+						pruneProgressBar.PerformStep();
+						secondaryFile = primaryFilenames[j];
+						secondaryFileStream = new FileStream(secondaryFile, FileMode.Open);
+						if (AreFileStreamsEqual(primaryFileStream, secondaryFileStream))
+						{
+							filesToPrune.Add(secondaryFile);
+							logWriter.WriteLine("[" + secondaryFile + "] is a copy of [" + primaryFile + "]\n");
+						}
+						secondaryFileStream.Close();
+					}
+					primaryFileStream.Close();
+				}
+			}
+			else
+			{
+				foreach (string primaryFile in primaryFilenames)
+				{
+					primaryFileStream = new FileStream(primaryFile, FileMode.Open);
+					foreach (string secondaryFile in secondaryFilenames)
+					{
+						pruneProgressBar.PerformStep();
+						secondaryFileStream = new FileStream(secondaryFile, FileMode.Open);
+						if (AreFileStreamsEqual(primaryFileStream, secondaryFileStream))
+						{
+							filesToPrune.Add(secondaryFile);
+							logWriter.WriteLine("[" + secondaryFile + "] is a copy of [" + primaryFile + "]\n");
+						}
+						secondaryFileStream.Close();
+					}
+					primaryFileStream.Close();
+				}
+			}
+
+			logWriter.Close();
+
+			//Do the pruning...
+			int numFilesToPrune = filesToPrune.Count();
+			bool doNothing = checkBoxDoNothing.Checked;
+			if (doNothing)
+			{
+				MessageBox.Show(this, "All done\n" + numFilesToPrune.ToString() + " files can be pruned");
+			}
+			else
+			{
+				MessageBox.Show(this, "All done\n" + numFilesToPrune.ToString() + " files were pruned");
+			}
+			pruneProgressBar.Value = 0;
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		static bool AreFileStreamsEqual(FileStream streamA, FileStream streamB)
+		{
+			long streamSizeA = streamA.Length;
+			long streamSizeB = streamB.Length;
+			if (streamSizeA != streamSizeB)
+			{
+				//different file sizes
+				return false;
+			}
+
+			streamA.Position = 0;
+			streamB.Position = 0;
+			for (int i = 0; i < streamSizeA; ++i)
+			{
+				if (streamA.ReadByte() != streamB.ReadByte())
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		static void GatherAllFiles(string sourcePath, ref List<string> fileList)
+		{
+			string nonPathName;
+			string[] files = Directory.GetFiles(sourcePath);
+			int numFiles = files.Length;
+			string sourceFileName;
+			for (int i = 0; i < numFiles; ++i)
+			{
+				nonPathName = Path.GetFileName(files[i]);
+				sourceFileName = sourcePath + "\\" + nonPathName;
+				fileList.Add(sourceFileName);
+			}
+
+			string[] folders = Directory.GetDirectories(sourcePath);
+			int numFolders = folders.Length;
+			string nestedSourcePath;
+			for (int i = 0; i < numFolders; ++i)
+			{
+				nonPathName = Path.GetFileName(folders[i]);
+				nestedSourcePath = sourcePath + "\\" + nonPathName;
+				GatherAllFiles(nestedSourcePath, ref fileList);
+			}
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		string ExtractPathFromDragEvent(DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+			{
+				string[] fileDirNames = e.Data.GetData(DataFormats.FileDrop) as string[];
+				string pathName = fileDirNames[0];
+				if (Directory.Exists(pathName))
+				{
+					return pathName;
+				}
+			}
+
+			return "";
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		//Returns true if duplicate path detected
+		bool DuplicatesAnyOtherTextBoxes(TextBox self, string pathName)
+		{
+			foreach (TextBox t in m_textBoxes)
+			{
+				if (self == t)
+				{
+					continue;
+				}
+
+				if (t.Text.Equals(pathName))
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		private void OnDragDrop(object sender, DragEventArgs e)
+		{
+			TextBox sendingTextBox = sender as TextBox;
+			if (null == sendingTextBox)
+			{
+				return;
+			}
+
+			string pathName = ExtractPathFromDragEvent(e);
+			if (pathName.Length > 0)
+			{
+				if (!DuplicatesAnyOtherTextBoxes(sendingTextBox, pathName))
+				{
+					sendingTextBox.Text = pathName;
+				}
+			}
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		private void OnDragEnter(object sender, DragEventArgs e)
+		{
+			TextBox sendingTextBox = sender as TextBox;
+			if (null == sendingTextBox)
+			{
+				return;
+			}
+
+			string pathName = ExtractPathFromDragEvent(e);
+			if (pathName.Length > 0)
+			{
+				if (DuplicatesAnyOtherTextBoxes(sendingTextBox, pathName))
+				{
+					e.Effect = DragDropEffects.None;
+				}
+				else
+				{
+					e.Effect = DragDropEffects.Copy;
+				}
+			}
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		void SetupPruneBasedOnPrimary()
+		{
+			string pathName = PrimaryPathTextbox.Text;
+			if ((0 == pathName.Length)
+				|| !Directory.Exists(pathName))
+			{
+				PruneDumpTextbox.Text = "";
+				return;
+			}
+			
+			//Auto-name the pruned folder
+			pathName += "_Pruned";
+			PruneDumpTextbox.Text = pathName;
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		private void PrimaryPathTextbox_TextChanged(object sender, EventArgs e)
+		{
+			if (!checkBoxWithinSelf.Checked)
+			{
+				return;
+			}
+
+			SetupPruneBasedOnPrimary();
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		void SetupPruneBasedOnSecondary()
+		{
+			string pathName = SecondaryPathTextbox.Text;
+			if ((0 == pathName.Length)
+				|| !Directory.Exists(pathName))
+			{
+				PruneDumpTextbox.Text = "";
+				return;
+			}
+
+			//Auto-name the pruned folder
+			pathName += "_Pruned";
+			if (!PrimaryPathTextbox.Text.Equals(pathName))
+			{
+				PruneDumpTextbox.Text = pathName;
+			}
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		private void SecondaryPathTextbox_TextChanged(object sender, EventArgs e)
+		{
+			if (checkBoxWithinSelf.Checked)
+			{
+				return;
+			}
+
+			SetupPruneBasedOnSecondary();
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		void OnWithinSelfChanged()
+		{
+			if (checkBoxWithinSelf.Checked)
+			{
+				PrimaryLabel.Text = "Folder to Prune (Drag a folder for easy path setting)";
+				SecondaryLabel.Hide();
+				SecondaryPathTextbox.Hide();
+				SetupPruneBasedOnPrimary();
+			}
+			else
+			{
+				PrimaryLabel.Text = "Primary Folder to Preserve (Drag a folder for easy path setting)";
+				SecondaryLabel.Show();
+				SecondaryPathTextbox.Show();
+				SetupPruneBasedOnSecondary();
+			}
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		private void checkBoxWithinSelf_CheckedChanged(object sender, EventArgs e)
+		{
+			OnWithinSelfChanged();
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		void OnDoNothingChanged()
+		{
+			if (checkBoxDoNothing.Checked)
+			{
+				MoveDupesButton.Text = "Check for Duplicates";
+			}
+			else
+			{
+				MoveDupesButton.Text = "Move Duplicates";
+			}
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		private void checkBoxDoNothing_CheckedChanged(object sender, EventArgs e)
+		{
+			OnDoNothingChanged();
+		}
+	}
+}
